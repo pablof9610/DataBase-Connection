e0e45c1e98c0486c8e1748744069f9578768cfa6
Pablo <pablof9610@gmail.com>
2023-09-27 08:43:57 -0300

    finishing create & adding cryptography

--- Operations.py
+++ Operations.py
@@ -1,12 +1,32 @@
+import mysql.connector
+from cryptography.fernet import Fernet
+
 class Operations:
     def __init__(self, con):
         self.con = con
 
-    def create(self):
-        self.con.conectar()
+    def create(self, name, bornyear, gender, email, password):
 
-        query = 'INSERT INTO teste.pessoa VALUES ()'
+        encrypted = bytes(password, encoding='utf-8')
+        encrypted = self.encrypt(encrypted)
+
+        try:
+            self.con.conectar()
+
+            query = "INSERT INTO pessoa (nome, idade, sexo, email, senha, crypt_key) VALUES (%s, %s, %s, %s, %s, %s)"
+            data_person = (name, bornyear, gender, email, encrypted[0], encrypted[1])
+            print(query)
+            cursor = self.con.conn.cursor()
+            cursor.execute(query, data_person)
+            self.con.conn.commit()
+
+            self.con.desconectar()
+        except mysql.connector.Error as error:
+            print('Ocorreu um erro na persistência de dados: {}'.format(error))
+        else:
+            print('Persistência realizada com sucesso.')
     
+    #in development
     def read(self):
         self.con.conectar()
 
@@ -21,42 +41,37 @@ class Operations:
         cursor.close()
         self.con.desconectar()
 
-    def find_passwd_by_id(self, id):
-        self.con.conectar()
-        query = f'SELECT senha FROM teste.pessoa WHERE matricula = {id}'
-        cursor = self.con.conn.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        result = result
-        self.con.desconectar()
-        return str(result)[3:str(result).rfind("'")].strip()
-    
-    def find_usremail_by_id(self, id):
-        self.con.conectar()
-        query = f'SELECT email FROM teste.pessoa WHERE matricula = {id}'
-        cursor = self.con.conn.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        result = result
-        self.con.desconectar()
-        return str(result)[3:str(result).rfind("'")].strip()
-    
     def find_usremail_by_email(self, email):
         self.con.conectar()
+
         query = f"SELECT email FROM teste.pessoa WHERE email = '{email}'"
         cursor = self.con.conn.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
         result = result
+
         self.con.desconectar()
         return str(result)[3:str(result).rfind("'")].strip()
     
     def find_passwd_by_email(self, email):
         self.con.conectar()
+
         query = f"SELECT senha FROM teste.pessoa WHERE email = '{email}'"
         cursor = self.con.conn.cursor()
         cursor.execute(query)
         result = cursor.fetchall()
         result = result
+
         self.con.desconectar()
         return str(result)[3:str(result).rfind("'")].strip()
+
+    def encrypt(self, password):
+        key = Fernet.generate_key()
+
+        # for while, i kept the key to decrypt the password
+        # on the "main" db because the purpose of this project is to study
+        list = [str(Fernet(key).encrypt(password))[2:str(Fernet(key).encrypt(password)).rfind("'")], key]
+        return list
+    
+    def decrypt(self, token, key) -> bytes:
+        return Fernet(key).decrypt(token)
